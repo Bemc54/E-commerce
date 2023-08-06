dc517c43f77f699e9803bf32ad8d8dec785f4fb2
Bemc54 <bemc54@gmail.com>
2023-08-06 11:06:11 -0600

    agregar proyecto

---
+++ bg.js
@@ -0,0 +1,41 @@
+addEventListener('DOMContentLoaded',()=>{
+    const burger=document.getElementById('burger')
+    const bar=document.querySelector('.bar')
+    burger.addEventListener('click', ()=>{
+        bar.classList.toggle('show')
+    })
+    
+    const luna=document.getElementById('luna')
+    const body=document.querySelector("body")
+    const header=document.querySelector("header")
+    luna.addEventListener('click',()=>{
+        body.classList.toggle("dark-mode")
+        header.classList.toggle("dark-mode2")
+        console.log(body)
+        console.log(header)
+    })
+
+    const dolphin=document.getElementById('dol')
+    dolphin.addEventListener('click',()=>{
+        body.classList.toggle("dolphin")
+        header.classList.toggle("dolphin2")
+        console.log(body)
+        console.log(header)
+    })
+
+    const zelda=document.getElementById('tri')
+    zelda.addEventListener('click',()=>{
+        body.classList.toggle("zelda")
+        header.classList.toggle("zelda2")
+        console.log(body)
+        console.log(header)
+    })
+
+    const sol=document.getElementById('sol')
+    sol.addEventListener('click',()=>{
+        body.classList.toggle("sol")
+        header.classList.toggle("sol2")
+        console.log(body)
+        console.log(header)
+    })
+})
